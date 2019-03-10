2b1547956a185de7beded05df7636897c2b02eca
Natima <natima@mail.com>
2019-03-10 19:56:16 +0100

    Initial Commit

---
+++ GameOn.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using GameOn.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameOn.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
