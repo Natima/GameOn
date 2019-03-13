e3746c9ba2ba1a51044d440864e6190efedc4b63
Natima <natima@mail.com>
2019-03-13 11:46:18 +0100

    Login and logout added

--- GameOn-SPA/src/app/app-routing.module.ts
+++ GameOn-SPA/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [];
 
+import { AuthService } from './_services/auth.service';
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- GameOn-SPA/src/app/nav/nav.component.ts
+++ GameOn-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
     selector: 'app-nav',
@@ -8,13 +9,30 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
     model: any = {};
 
-    constructor() { }
+    constructor(private authService: AuthService) { }
 
     ngOnInit() {
     }
 
     login() {
-        console.log(this.model);
+        // returning observable, need to subscribe
+        this.authService.login(this.model).subscribe(next => {
+            console.log('Logged in successfully');
+        }, error => {
+            console.log('Failed to login, check your username or password');
+        });
     }
 
+    // Logged In method
+    loggedIn() {
+        const token = localStorage.getItem('token');
+        // will return true or false value, if it's something in token it will return true 
+        // if var empty return false
+        return !!token;
+    }
+    // logout method
+    logout() {
+        localStorage.removeItem('token');
+        console.log('logged out')
+    }
 }
