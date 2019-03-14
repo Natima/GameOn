2a2fac8f4ceffb019626db7ca3db9ac3bdc0518a
Natima <natima@mail.com>
2019-03-14 16:05:59 +0100

    alertify added

--- GameOn-SPA/src/app/nav/nav.component.ts
+++ GameOn-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
     selector: 'app-nav',
@@ -9,7 +10,7 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
     model: any = {};
 
-    constructor(private authService: AuthService) { }
+    constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
     ngOnInit() {
     }
@@ -17,9 +18,9 @@ export class NavComponent implements OnInit {
     login() {
         // returning observable, need to subscribe
         this.authService.login(this.model).subscribe(next => {
-            console.log('Logged in successfully');
+            this.alertify.success('Logged in successfully');
         }, error => {
-            console.log(error);
+            this.alertify.error(error);
         });
     }
 
@@ -33,6 +34,6 @@ export class NavComponent implements OnInit {
     // logout method
     logout() {
         localStorage.removeItem('token');
-        console.log('logged out')
+        this.alertify.message('You are now logged out');
     }
 }
