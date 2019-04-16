fc0fa78bd1500de89c4d246cc60fb264bee95197
Natima <natima@mail.com>
2019-04-16 09:57:43 +0200

    Standard photo for new registered

--- GameOn-SPA/src/app/nav/nav.module.ts
+++ GameOn-SPA/src/app/nav/nav.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 // This Module's Components
 import { NavComponent } from './nav.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
-
+        BrowserModule,
+        RouterModule,
+        FormsModule,
     ],
     declarations: [
         NavComponent,
