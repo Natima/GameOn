49b02da03c44eaa98f6a2d105630b2b03fab1ae0
Natima <natima@mail.com>
2019-04-19 15:59:56 +0200

    Paginator added to API

--- GameOn.API/Controllers/UsersController.cs
+++ GameOn.API/Controllers/UsersController.cs
@@ -26,10 +26,14 @@ namespace GameOn.API.Controllers
 
         }
         [HttpGet]
-        public async Task<IActionResult> GetUsers()
+        public async Task<IActionResult> GetUsers([FromQuery]UserParams userParams)
         {
-            var users = await _repo.GetUsers();
+            var users = await _repo.GetUsers(userParams);
             var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+
+            Response.AddPaginatior(users.CurrentPage, users.PageSize,
+            users.TotalCount, users.TotalPages);
+
             return Ok(usersToReturn);
         }
         [HttpGet("{id}", Name = "GetUser")]
--- GameOn.API/Data/IDatingRepository.cs
+++ GameOn.API/Data/IDatingRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using GameOn.API.Helpers;
 using GameOn.API.Models;
 
 namespace GameOn.API.Data
@@ -9,7 +10,7 @@ namespace GameOn.API.Data
          void Add<T>(T entity) where T: class;
          void Delete<T>(T entity) where T: class;
          Task<bool> SaveAll();
-         Task<IEnumerable<User>> GetUsers();
+         Task<PagedList<User>> GetUsers(UserParams userParams);
          Task<User> GetUser(int id);
          Task<Photo> GetPhoto(int id);
          Task<Photo> GetMainPhotoForUser(int id);
