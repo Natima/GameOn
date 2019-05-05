b370f29ed2e36ef99500898de7c9128a8b5b07c5
Natima <natima@mail.com>
2019-05-05 16:40:41 +0200

    Theme change and Like function started

---
+++ GameOn.API/Models/Like.cs
@@ -0,0 +1,10 @@
+namespace GameOn.API.Models
+{
+    public class Like
+    {
+        public int LikerId { get; set; }
+        public int LikeeId { get; set; }
+        public User Liker { get; set; }
+        public User Likee { get; set; }
+    }
+}
