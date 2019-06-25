f7784782b33fdb8198ad58f549a3b5eb29adbd31
Natima <natima@mail.com>
2019-06-25 12:06:07 +0200

    Message Entity added to database

--- GameOn.API/Migrations/20190625100214_MessageEntityAdded.Designer.cs
+++ GameOn.API/Migrations/20190625100214_MessageEntityAdded.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using GameOn.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GameOn.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190625100214_MessageEntityAdded")]
+    partial class MessageEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
