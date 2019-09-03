371ce1b7cfacc1077cc773c8c4c96e1b51a4c7f7
Natima <natima@mail.com>
2019-09-03 15:28:19 +0200

    Repository method for inbox and outbox added

--- GameOn.API/Data/IDatingRepository.cs
+++ GameOn.API/Data/IDatingRepository.cs
@@ -16,7 +16,7 @@ namespace GameOn.API.Data
          Task<Photo> GetMainPhotoForUser(int id);
          Task<Like> GetLike(int userId, int recipientId);
          Task<Message> GetMessage(int id);
-         Task<PagedList<Message>> GetMessageForUser();
+         Task<PagedList<Message>> GetMessageForUser(MessageParams messageParams);
          Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId);
     }
 }
